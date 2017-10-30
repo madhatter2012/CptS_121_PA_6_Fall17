1ddd3925f86e10fb83762b1fd6e595bc8af95087
C Smith <madhatter2012@gmail.com>
2017-10-29 20:28:51 -0700

    1st commit of PA 6 for CptS 121 Fall 2017

---
+++ CptS_121_PA_6_Fall17/main.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+* Programmer: Michael Smith														 *
+* Class CptS 121, Fall 2017; Lab Section 21										 *
+* Programming Assignment: PA5													 *
+* Date: October 18, 2017														 *
+* Description: This program processes numbers, corresponding to student		     *
+* records read in from a file, and writes the required results to an output file.*
+**********************************************************************************/
+
+
+#include "battleship.h"
+#ifndef Yahtzee_H
+#define Yahtzee_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // for printf and scanf
+#include <stdlib.h> //for standard library
+#include <math.h> //for math calculations
+#include <time.h> //for random num gen
+
+
+#endif
